29013e275bb5cbd1fa6dc2fb8b1fbb5e6e6cc3ef
maria6585 <exceptional6585@naver.com>
2021-11-17 18:29:16 +0900

    멀티이미지

--- main/admin.py
+++ main/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import Post
+from .models import Category, Post
 #from .models import Category
 
 # Register your models here.
 
 admin.site.register(Post)
+admin.site.register(Category)
 
 
 #admin.site.register(Category)
