6a6e2a55ac35d58f1243d0f7661afe7b8ccfc364
phaesol <parkhaesol0227@likelion.org>
2021-11-18 20:27:46 +0900

    post update,delete detail, country func, multiimage, category 별 post_list

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,7 @@
 from os import name
 from django.contrib import admin
 from django.urls import path
-from .views import delete, main, create, detail, update, delete
+from .views import *
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -18,6 +18,7 @@ urlpatterns = [
     path('delete/<int:post_id>', delete, name="delete"),
     path('update/<int:post_id>',update, name="update"),
     path('detail/<int:post_id>', detail, name="detail"),
+    path('category/<int:category_id>',category,name="category")
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
 
