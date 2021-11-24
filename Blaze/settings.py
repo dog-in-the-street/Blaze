1f80298fb85224d60413e5f43cfa6e98c39a974c
rkddnr330 <rkddnr330@nate.com>
2021-11-24 21:20:58 +0900

    intro and pull

--- Blaze/settings.py
+++ Blaze/settings.py
@@ -42,7 +42,9 @@ SECRET_KEY = get_secret("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "ec2-3-34-99-30.ap-northeast-2.compute.amazonaws.com"
+    "ec2-3-34-99-30.ap-northeast-2.compute.amazonaws.com",
+    "127.0.0.1:8000/",
+    "127.0.0.1",
 ]
 
 
